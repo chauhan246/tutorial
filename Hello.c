4127b096584bf993d02a4820315a6cbee2b7b651
chauhan246 <156046816+chauhan246@users.noreply.github.com>
2024-01-22 16:35:41 -0500

    first file uploaded c

---
+++ Hello.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main () {
+
+printf("Hello C \n");
+
+int a;
+int b;
+
+printf("Enter number: ");
+scanf("%d", &a);
+
+printf("Enter number: ");
+scanf("%d", &b);
+
+printf("The result is: %d", a + b);
+
+
+    return 0;
+}
